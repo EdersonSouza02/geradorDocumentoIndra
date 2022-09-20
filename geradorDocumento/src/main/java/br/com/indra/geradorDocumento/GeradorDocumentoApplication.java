2ce67a8189da76f7c9814dccf6d89b536bfe7892
Ederson <edersonfreitas2012@hotmail.com>
2022-09-20 10:12:43 -0300

    Projeto Spring gerado

---
+++ geradorDocumento/src/main/java/br/com/indra/geradorDocumento/GeradorDocumentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.indra.geradorDocumento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorDocumentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorDocumentoApplication.class, args);
+	}
+
+}
---
+++ geradorDocumento/src/test/java/br/com/indra/geradorDocumento/GeradorDocumentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.indra.geradorDocumento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorDocumentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
